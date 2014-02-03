b8fb0492f607660f885c2648113d7b28f37f74eb
Metrime <Michi@Michaels-Mac-mini.local>
2014-02-03 15:09:52 +0100

    Adding uglifier

--- DOM/build-all/gulpfile.js
+++ DOM/build-all/gulpfile.js
@@ -1,8 +1,12 @@
 var gulp = require("gulp");
 var concat = require("gulp-concat");
+var footer = require("gulp-footer");
+var uglify = require("gulp-uglify");
 
 gulp.task('default',function(){
 	gulp.src(['../core.js','../*.js','!../all.js'])
+	.pipe(footer(';'))
 	.pipe(concat('all.js'))
+	.pipe(uglify())
 	.pipe(gulp.dest('../'))
 });
