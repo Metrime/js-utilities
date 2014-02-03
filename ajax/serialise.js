c1d07c8b65d4b4fd8c07fea1b04f42493b628f27
Metrime <Michi@Michaels-Mac-mini.local>
2014-02-03 20:32:51 +0100

    -

---
+++ ajax/serialise.js
@@ -0,0 +1,7 @@
+_.serialise = function(obj){
+  var serialised = '';
+  for (key in obj){
+    serialised += '&'+key+'='+obj[key];
+  }
+  return serialised;
+}
---
+++ dates & times/normalise.js
@@ -0,0 +1,26 @@
+_.time.normalise = function(date){
+	date = date ? date : new Date();
+
+	function fix(number){
+		return number < 10 ? '0'+number : number;
+	}
+
+	var hours = fix(date.getHours()),
+		minutes = fix(date.getMinutes()),
+		seconds = fix(date.getSeconds()),
+
+		day = fix(date.getDate()),
+		month = fix((date.getMonth()+1)),
+		year = date.getFullYear();
+
+	return {
+		date: [day,month,year].join('.'),
+		time: [hours,minutes,seconds].join(':'),
+		h: hours,
+		m: minutes,
+		s: seconds,
+		d: day,
+		m: month,
+		y: year
+	}
+}
