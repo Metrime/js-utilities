6e79207b1310a202dde2ea07700d90b456c3da32
Metrime <Michi@Michaels-Mac-mini.local>
2014-02-05 16:19:18 +0100

    switching to microAjax

--- utils/ajax.js
+++ utils/ajax.js
@@ -1,12 +1,3 @@
 require('utils/core')
 
-_.ajax(url, callback, data, x) {
-	x = new(this.XMLHttpRequest || ActiveXObject)('MSXML2.XMLHTTP.3.0');
-	x.open(data ? 'POST' : 'GET', url, 1);
-	x.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-	x.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-	x.onreadystatechange = function () {
-		x.readyState > 3 && callback && callback(x.responseText, x);
-	};
-	x.send(data)
-};
+_.ajax = function(B,A){this.bindFunction=function(E,D){return function(){return E.apply(D,[D])}};this.stateChange=function(D){if(this.request.readyState==4){this.callbackFunction(this.request.responseText)}};this.getRequest=function(){if(window.ActiveXObject){return new ActiveXObject("Microsoft.XMLHTTP")}else{if(window.XMLHttpRequest){return new XMLHttpRequest()}}return false};this.postBody=(arguments[2]||"");this.callbackFunction=A;this.url=B;this.request=this.getRequest();if(this.request){var C=this.request;C.onreadystatechange=this.bindFunction(this.stateChange,this);if(this.postBody!==""){C.open("POST",B,true);C.setRequestHeader("X-Requested-With","XMLHttpRequest");C.setRequestHeader("Content-type","application/x-www-form-urlencoded");C.setRequestHeader("Connection","close")}else{C.open("GET",B,true)}C.send(this.postBody)}};
